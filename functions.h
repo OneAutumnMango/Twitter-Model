3edc84839d4c5b4fedabf8519837840fc21ed19b
BreadAccountant <connollyfamily2014@gmail.com>
2022-04-05 15:32:14 +0100

    get_username to header

--- functions.h
+++ functions.h
@@ -3,6 +3,7 @@
 user initialise_user(char username[USERNAME_LENGTH]);
 tweet initialise_tweet(size_t userID);
 twitter initialise_twitter(void);
+char *get_username(twitter twitter);
 
 
 /* returns true if item is in list of length length -> is_in(item, list, length) */
