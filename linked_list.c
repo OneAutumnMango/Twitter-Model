b16cb9cdc8f4348f7abe837497d9edd03c02f047
BreadAccountant <connollyfamily2014@gmail.com>
2022-04-12 14:41:32 +0100

    added remove_users_tweets

--- linked_list.c
+++ linked_list.c
@@ -6,3 +6,22 @@ Node* push(tweet new_tweet, Node *previous_node) {  /* returns new start of link
     new_node->next_node = previous_node;
     return new_node;  
 }
+
+void remove_users_tweets(twitter *twitter) {     /* removes all the current users tweets from linked list */
+    Node* previous = twitter->most_recent_tweet;
+    Node* current = twitter->most_recent_tweet;
+    while(current->next_node != NULL) { /* go through all the tweets */
+        if (current->tweet.userID == twitter->current_userID) { /* if the author is the current user */
+            previous->next_node = current;      /* remove the tweet from the list */
+
+            if(current == twitter->most_recent_tweet) {     /* if the tweet is the newest tweet */
+                twitter->most_recent_tweet = current->next_node;    /* remove the tweet from the front */
+            }
+            free(previous);
+        }
+        else {
+            previous = current;     /* move onto the next node */
+        }
+        current = current->next_node;   /* move onto the next node */
+    }
+}
