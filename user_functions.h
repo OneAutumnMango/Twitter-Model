94db1e3357ba9370be6b00ec58988f776a265b30
OneAutumnMango <mangozoldyck@gmail.com>
2022-05-01 23:40:20 +0100

    fixed getNewsFeed

--- user_functions.h
+++ user_functions.h
@@ -3,7 +3,7 @@
  * This function gets a new username, creates a twitter account with that
  * username. It returns the userID of the new twitter account.
  */
-size_t create_account(twitter *twitter);
+void create_account(twitter *twitter);
 
 void login(twitter *twitter);
 void postTweet(twitter *twitter);
