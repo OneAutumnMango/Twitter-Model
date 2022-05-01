d71c00a93d6d4a6bb964ee4cd98f14b2cc58c31e
BreadAccountant <connollyfamily2014@gmail.com>
2022-05-02 00:47:56 +0100

    for david <3

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 int main(void) {
 
     twitter twitter = initialise_twitter();
-    printf("%u %u\n", twitter.current_user->user.userID, twitter.user_count);
     create_account(&twitter);
-    printf("%u %u\n", twitter.current_user->user.userID, twitter.user_count);
     create_account(&twitter);
-    printf("%u %u\n", twitter.current_user->user.userID, twitter.user_count);
+    printf("%u %u\n", twitter.current_user->user->userID, twitter.user_count);
+    create_account(&twitter);
+    printf("%u %u\n", twitter.current_user->user->userID, twitter.user_count);
 //    login(&twitter);
 //    printf("%u %u\n", twitter.current_userID, twitter.user_count);
 //    list_users(&twitter);
@@ -27,12 +27,12 @@ int main(void) {
     postTweet(&twitter);
     postTweet(&twitter);
     getNewsfeed(&twitter);
-    printf("%u %u\n", twitter.current_user->user.userID, twitter.user_count);
+    printf("%u %u\n", twitter.current_user->user->userID, twitter.user_count);
 
-    printf("%u %u\n", twitter.current_user->user.userID, twitter.user_count);
+    printf("%u %u\n", twitter.current_user->user->userID, twitter.user_count);
 //    getNewsfeed(&twitter);
-    printf("%u %u\n", twitter.current_user->user.userID, twitter.user_count);
-    twitter.current_user->user.userID = 1;
+    printf("%u %u\n", twitter.current_user->user->userID, twitter.user_count);
+    twitter.current_user->user->userID = 1;
     postTweet(&twitter);
     getNewsfeed(&twitter);
 
